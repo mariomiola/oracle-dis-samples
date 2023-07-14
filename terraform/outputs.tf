3e6e721d7d600f235eff3e8c041d68624af24e0a
Mario Miola <108755375+mariomiola@users.noreply.github.com>
2023-07-14 16:23:27 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
+## Copyright (c) 2022, Oracle and/or its affiliates.
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
 output "project_key" {
   description = ""
   value       = oci_dataintegration_workspace_project.dis_workspace_project.key
