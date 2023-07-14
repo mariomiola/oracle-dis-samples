a9cebdfecc57570b09efed40dafd0b10d3d42154
Mario Miola <108755375+mariomiola@users.noreply.github.com>
2023-07-14 16:23:08 -0700

    Update dis.tf

--- terraform/dis.tf
+++ terraform/dis.tf
@@ -1,3 +1,6 @@
+## Copyright (c) 2022, Oracle and/or its affiliates.
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
 resource "oci_dataintegration_workspace" "dis_workspace" {
     #depends_on = [oci_identity_group.DISManage]
     #Required
