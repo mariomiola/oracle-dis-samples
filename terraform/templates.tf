154e602dcdfd2559efc30a6556a5c32f32bb8cb7
Mario Miola <108755375+mariomiola@users.noreply.github.com>
2023-07-14 16:24:00 -0700

    Update templates.tf

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -1,3 +1,6 @@
+## Copyright (c) 2022, Oracle and/or its affiliates.
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
 resource "null_resource" "get_dis_template_keys" {
   depends_on = [oci_dataintegration_workspace.dis_workspace, oci_dataintegration_workspace_project.dis_workspace_project]
   provisioner "local-exec" {
